b31879728634d27087afc82d533fc0a5dbbc4fb8
marzenie <marszalek_szymon@tlen.pl>
2023-12-09 17:04:16 +0100

    add menu, add type option (only visual)

--- functions/format.py
+++
@@ -1,4 +0,0 @@
-from colorama import init, Fore, Back, Style
-def format_text(text, color=Fore.WHITE, background=None, style=None):
-    formatted_text = f"{color}{background}{style}{text}{Style.RESET_ALL}"
-    return formatted_text
